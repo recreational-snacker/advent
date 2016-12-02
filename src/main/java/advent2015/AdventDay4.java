d6b8d287c7c9f0fb44c490cf704cda6d398fa210
Deesha Singh <missdeeshasingh@gmail.com>
2016-12-02 22:20:35 +0200

    day4year2015

---
+++ src/main/java/advent2015/AdventDay4.java
@@ -0,0 +1,39 @@
+package advent2015;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Deesha Singh on 2016/12/02.
+ */
+public class AdventDay4 {
+
+    private final static String MD5 = "MD5";
+
+    public static String solution(String input, String matcher) {
+        String md5 = null;
+        for (int i = 0; ; i++) {
+            String attempt = input+i;
+            md5 = createMd5(attempt);
+            if (md5.startsWith(matcher)) {
+                return String.valueOf(i);
+            }
+        }
+    }
+
+    public static String createMd5(String input) {
+        String md5 = null;
+        if(null == input) return null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance(MD5);
+            digest.update(input.getBytes(), 0, input.length());
+            BigInteger integer = new BigInteger(1, digest.digest());
+            md5 = String.format("%1$032x", integer);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return md5;
+    }
+}
---
+++ src/test/java/advent2015/AdventDay4Test.java
@@ -0,0 +1,43 @@
+package advent2015;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Deesha Singh on 2016/12/02.
+ */
+public class AdventDay4Test {
+
+    @Test
+    public void testMD5CreationOfExampl1() throws Exception {
+        String input = "abcdef609043";
+        String md5 = AdventDay4.createMd5(input);
+        System.out.println(md5);
+        Assert.assertEquals("000001dbbfa3a5c83a2d506429c7b00e", md5);
+    }
+
+    @Test
+    public void testMD5CreationOfExampl2() throws Exception {
+        String input = "pqrstuv1048970";
+        String md5 = AdventDay4.createMd5(input);
+        System.out.println(md5);
+        Assert.assertEquals("000006136ef2ff3b291c85725f17325c", md5);
+    }
+
+    @Test
+    public void testMSolution() throws Exception {
+        String input = "bgvyzdsv";
+        String matcher = "00000";
+        String md5 = AdventDay4.solution(input, matcher);
+        System.out.println(md5);
+    }
+
+    @Test
+    public void testMSolution2() throws Exception {
+        String input = "bgvyzdsv";
+        String matcher = "000000";
+        String md5 = AdventDay4.solution(input, matcher);
+        System.out.println(md5);
+    }
+
+}
