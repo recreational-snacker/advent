c2ed27d2e6d81cbbefeb63aadb8bc661590b694d
deesha.singh <deesha.singh@symbiotics.co.za>
2018-12-23 08:51:11 +0200

    days

---
+++ src/test/java/advent2018/Advent2018Day13Test.java
@@ -0,0 +1,11 @@
+package advent2018;
+
+public class Advent2018Day13Test {
+
+    private String testInput = "/->-\\        \n" +
+            "|   |  /----\\\n" +
+            "| /-+--+-\\  |\n" +
+            "| | |  | v  |\n" +
+            "\\-+-/  \\-+--/\n" +
+            "  \\------/   \n";
+}
